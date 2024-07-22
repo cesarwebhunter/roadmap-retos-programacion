54cdad8dab7da04eab03718ff23a30083433ecc1
cesarwebhunter <cesarwebhunter@gmail.com>
2024-07-22 14:02:04 +0200

    Reto 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/CesarWebHunter.js
@@ -0,0 +1,24 @@
+// URL de JavaScript: https://www.javascript.com/
+
+// Comentario simple
+
+/*
+Comentario
+Multilínea
+*/
+
+//Variable y constante
+let myVariable = "César";
+const myConstant = 9.8;
+
+//Tipos de variables.
+
+let cadenaDeTexto = "Esto es una cadena de texto";
+
+let number10 = 10;
+
+let isActive = true;
+
+// Imprimiendo Hola JS:
+
+console.log("Hola, JS!")
